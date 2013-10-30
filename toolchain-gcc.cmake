c49c9576a25b98c971c8640da8b0ef290074d862
Nils Larsson <nisselarsson000@gmail.com>
2013-10-30 21:17:15 +0100

    Added various GCC optimization options.

--- toolchain-gcc.cmake
+++ toolchain-gcc.cmake
@@ -20,8 +20,30 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-write-strings")
 # Omit "unused parameter 'foo'" warnings.
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-unused-parameter")
 
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,-O1 -Wl,--as-needed")
+
+option(FSO_USE_LTO "Build using GCC's Link Time Optimization feature" FALSE)
+if(FSO_USE_LTO)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -flto")
+	set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -flto")
+	message(STATUS "FSO_USE_LTO is enabled. Linking will probably take a LONG time to complete.")
+endif(FSO_USE_LTO)
+
+option(FSO_USE_GRAPHITE "Build using GCC's Graphite feature" FALSE)
+if(FSO_USE_GRAPHITE)
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fgraphite-identity -ftree-loop-linear -floop-strip-mine -floop-block -floop-nest-optimize")
+endif(FSO_USE_GRAPHITE)
+
+if(FSO_USE_NATIVE)
+	string(REGEX REPLACE "-march=[^ ]*" "-march=native" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+	string(REGEX REPLACE "-mtune=[^ ]*\ " "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+	string(REGEX REPLACE "-mss[^ ]*\ " "" CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+	message(STATUS "FSO_USE_NATIVE is enabled. All instruction subsets supported by the local machine will be used, the resulting binary might not run on different machines.")
+endif(FSO_USE_NATIVE)
+
 set(CMAKE_CXX_FLAGS_RELEASE "-O2")
+set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-Wl,--strip-all")
 
 set(CMAKE_CXX_FLAGS_DEBUG "-O0 -g -Wextra -Wshadow")
-set(CMAKE_EXE_LINKER_FLAGS "-Wl,-O1 -Wl,--as-needed")
-set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-Wl,--strip-all")
+
+
