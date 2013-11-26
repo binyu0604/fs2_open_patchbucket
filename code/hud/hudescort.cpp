binyu0604/fs2_open_patchbucket

commit 0d6e8244bc539617dd033b38cfed27984c218633

Fixed some improper initialization of escort list hit_infos.
git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10170 387891d4-d844-0410-90c0-e4c51a9137d3

code/hud/hudescort.cpp
@@ -563,7 +563,7 @@ void hud_create_complete_escort_list(escort_info *escorts, int *num_escorts)
 				escorts[*num_escorts].obj_signature = -1;
 				escorts[*num_escorts].priority = -1;
 				escorts[*num_escorts].np_id = Net_players[idx].player_id;
-				shield_info_reset(NULL, &escorts[*num_escorts].hit_info);
+				shield_info_reset(&Objects[Net_players[idx].mplayer->objnum], &escorts[*num_escorts].hit_info);
 				(*num_escorts)++;
 			}
 		}
@@ -723,7 +723,7 @@ void merge_escort_lists(escort_info *complete_escorts, int num_complete_escorts)
 				continue;
 			}
 			if ( !valid_hit_info[i] ) {
-				shield_info_reset(NULL, &Escort_ships[i].hit_info);
+				shield_info_reset(&Objects[objnum], &Escort_ships[i].hit_info);
 			}	
 		}
 	}

commit 70b0b270bd55ad28e077291251c501acb2fa9867

Resolve compilation failure introduced by r10170
git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10171 387891d4-d844-0410-90c0-e4c51a9137d3

code/hud/hudescort.cpp
@@ -563,7 +563,7 @@ void hud_create_complete_escort_list(escort_info *escorts, int *num_escorts)
 				escorts[*num_escorts].obj_signature = -1;
 				escorts[*num_escorts].priority = -1;
 				escorts[*num_escorts].np_id = Net_players[idx].player_id;
-				shield_info_reset(&Objects[Net_players[idx].mplayer->objnum], &escorts[*num_escorts].hit_info);
+				shield_info_reset(&Objects[Net_players[idx].m_player->objnum], &escorts[*num_escorts].hit_info);
 				(*num_escorts)++;
 			}
 		}