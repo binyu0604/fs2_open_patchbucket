binyu0604/fs2_open_patchbucket

commit 4753ed85e927e326ced5ff680d16c1d8460b3d5e

Ported the $On Action and $On Action Stopped scripting hooks from zookeeper branch. Also bumps the char limit for conditions from 32 to 64.
git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10254 387891d4-d844-0410-90c0-e4c51a9137d3

code/controlconfig/controlsconfigcommon.cpp
@@ -375,7 +375,10 @@ void set_modifier_status()
 	}
 }
 
-int translate_key_to_index(char *key)
+// If find_override is set to true, then this returns the index of the action
+// which has been bound to the given key. Otherwise, the index of the action
+// which has the given key as its default key will be returned.
+int translate_key_to_index(const char *key, bool find_override)
 {
 	int i, index = -1, alt = 0, shift = 0, max_scan_codes;
 
@@ -430,10 +433,19 @@ int translate_key_to_index(char *key)
 			index |= KEY_ALTED;
 
 		// convert scancode to Control_config index
-		for (i=0; i<CCFG_MAX; i++) {
-			if (Control_config[i].key_default == index) {
-				index = i;
-				break;
+		if (find_override) {
+			for (i=0; i<CCFG_MAX; i++) {
+				if (Control_config[i].key_id == index) {
+					index = i;
+					break;
+				}
+			}
+		} else {
+			for (i=0; i<CCFG_MAX; i++) {
+				if (Control_config[i].key_default == index) {
+					index = i;
+					break;
+				}
 			}
 		}
 
@@ -458,7 +470,7 @@ char *translate_key(char *key)
 
 	static char text[40] = {"None"};
 
-	index = translate_key_to_index(key);
+	index = translate_key_to_index(key, true);
 	if (index < 0) {
 		return NULL;
 	}
@@ -536,8 +548,10 @@ void control_config_common_load_overrides();
 // initialize common control config stuff - call at game startup after localization has been initialized
 void control_config_common_init()
 {
-	for (int i=0; i<CCFG_MAX; i++)
+	for (int i=0; i<CCFG_MAX; i++) {
 		Control_config[i].disabled = false;
+		Control_config[i].continuous_ongoing = false;
+	}
 
     control_config_common_load_overrides();
 	if(Lcl_gr){

code/controlconfig/controlsconfig.cpp
@@ -28,6 +28,7 @@
 #include "globalincs/alphacolors.h"
 #include "network/multi_pmsg.h"
 #include "network/multiutil.h"
+#include "parse/scripting.h"
 
 
 #ifndef NDEBUG
@@ -2132,6 +2133,8 @@ float check_control_timef(int id)
 
 	// first, see if control actually used (makes sure modifiers match as well)
 	if (!check_control(id)) {
+		Control_config[id].continuous_ongoing = false;
+
 		return 0.0f;
 	}
 
@@ -2146,6 +2149,11 @@ float check_control_timef(int id)
 	}
 
 	if (t1 + t2) {
+		// We want to set this to true only after visiting control_used() (above)
+		// to allow it to tell the difference between an ongoing continuous action
+		// started before and a continuous action being started right now.
+		Control_config[id].continuous_ongoing = true;
+
 		return t1 + t2;
 	}
 
@@ -2251,6 +2259,17 @@ int check_control(int id, int key)
 		return 1;
 	}
 
+	if (Control_config[id].continuous_ongoing) {
+		// If we reach this point, then it means this is a continuous control
+		// which has just been released
+
+		Script_system.SetHookVar("Action", 's', Control_config[id].text);
+		Script_system.RunCondition(CHA_ONACTIONSTOPPED, '\0', NULL, NULL, id);
+		Script_system.RemHookVar("Action");
+
+		Control_config[id].continuous_ongoing = false;
+	}
+
 	return 0;
 }
 
@@ -2306,6 +2325,7 @@ void control_get_axes_readings(int *h, int *p, int *b, int *ta, int *tr)
 	return;
 }
 
+int Last_frame_timestamp;
 void control_used(int id)
 {
 	// if we have set this key to be ignored, ignore it
@@ -2313,7 +2333,20 @@ void control_used(int id)
 		return;
 	}
 
-	Control_config[id].used = timestamp();
+	// This check needs to be done because the control code might call this function more than once per frame,
+	// and we don't want to run the hooks more than once per frame
+	if (Control_config[id].used < Last_frame_timestamp) {
+		if (!Control_config[id].continuous_ongoing) {
+			Script_system.SetHookVar("Action", 's', Control_config[id].text);
+			Script_system.RunCondition(CHA_ONACTION, '\0', NULL, NULL, id);
+			Script_system.RemHookVar("Action");
+
+			if (Control_config[id].type == CC_TYPE_CONTINUOUS)
+				Control_config[id].continuous_ongoing = true;
+		}
+
+		Control_config[id].used = timestamp();
+	}
 }
 
 void control_config_clear_used_status()

code/controlconfig/controlsconfig.h
@@ -69,6 +69,7 @@ typedef struct config_item {
 	short joy_id;			//!< joystick button bound to action
 	int used;				//!< has control been used yet in mission?  If so, this is the timestamp
 	bool disabled;			//!< whether this action should be available at all
+	bool continuous_ongoing;//!< whether this action is a continuous one and is currently ongoing
 } config_item;
 
 /*!
@@ -301,7 +302,7 @@ void control_config_close();
 void control_config_cancel_exit();
 
 void control_config_reset_defaults(int presetnum=-1);
-int translate_key_to_index(char *key);
+int translate_key_to_index(const char *key, bool find_override=true);
 char *translate_key(char *key);
 char *textify_scancode(int code);
 float check_control_timef(int id);

code/freespace2/freespace.cpp
@@ -4757,6 +4757,7 @@ void game_set_frametime(int state)
 
 	Last_time = thistime;
 	//mprintf(("Frame %i, Last_time = %7.3f\n", Framecount, f2fl(Last_time)));
+	Last_frame_timestamp = timestamp();
 
 	flFrametime = f2fl(Frametime);
 	timestamp_inc(flFrametime);

code/freespace2/freespace.h
@@ -36,6 +36,7 @@ extern fix Frametime;
 extern float flRealframetime;
 extern float flFrametime;
 extern fix Missiontime;
+extern int Last_frame_timestamp; // A timestamp for when the previous frame ended
 
 // 0 - 4
 extern int Game_skill_level;

code/globalincs/globals.h
@@ -22,6 +22,7 @@
 #define	FILESPEC_LENGTH			64
 #define	MESSAGE_LENGTH			512
 #define TRAINING_MESSAGE_LENGTH	512
+#define CONDITION_LENGTH		64
 
 // from missionparse.h
 #define MISSION_DESC_LENGTH		512

code/parse/scripting.cpp
@@ -38,6 +38,7 @@ flag_def_list Script_conditions[] =
 	{"Ship type",	CHC_SHIPTYPE,		0},
 	{"Weapon class",CHC_WEAPONCLASS,	0},
 	{"KeyPress",	CHC_KEYPRESS,		0},
+	{"Action",		CHC_ACTION,			0},
 	{"Version",		CHC_VERSION,		0},
 	{"Application",	CHC_APPLICATION,	0}
 };
@@ -50,6 +51,8 @@ flag_def_list Script_actions[] =
 	{"On Splash Screen",		CHA_SPLASHSCREEN,	0},
 	{"On State Start",			CHA_ONSTATESTART,	0},
 	{"On Frame",				CHA_ONFRAME,		0},
+	{"On Action",				CHA_ONACTION,		0},
+	{"On Action Stopped",		CHA_ONACTIONSTOPPED,0},
 	{"On Key Pressed",			CHA_KEYPRESSED,		0},
 	{"On Key Released",			CHA_KEYRELEASED,	0},
 	{"On Mouse Moved",			CHA_MOUSEMOVED,		0},
@@ -437,6 +440,17 @@ bool ConditionedHook::ConditionsValid(int action, object *objp, int more_data)
 						return false;
 					//WMC - could be more efficient, but whatever.
 					if(stricmp(textify_scancode(Current_key_down), scp->data.name))
+						return false;
+					break;
+				}
+			case CHC_ACTION:
+				{
+					if(gameseq_get_depth() < 0)
+						return false;
+
+					int action_index = more_data;
+
+					if (action_index <= 0 || stricmp(scp->data.name, Control_config[action_index].text))
 						return false;
 					break;
 				}
@@ -1327,7 +1341,7 @@ bool script_state::ParseCondition(const char *filename)
 			case CHC_VERSION:
 			case CHC_APPLICATION:
 			default:
-				stuff_string(sct.data.name, F_NAME, NAME_LENGTH);
+				stuff_string(sct.data.name, F_NAME, CONDITION_LENGTH);
 				break;
 		}
 

code/parse/scripting.h
@@ -34,8 +34,9 @@ struct image_desc
 #define CHC_WEAPONCLASS		6
 #define CHC_OBJECTTYPE		7
 #define CHC_KEYPRESS		8
-#define CHC_VERSION			9
-#define CHC_APPLICATION		10
+#define CHC_ACTION			9
+#define CHC_VERSION			10
+#define CHC_APPLICATION		11
 
 //Actions
 #define CHA_NONE			-1
@@ -71,6 +72,8 @@ struct image_desc
 #define CHA_SECONDARYFIRE	29
 #define CHA_ONSHIPARRIVE	30
 #define CHA_COLLIDEBEAM		31
+#define CHA_ONACTION		32
+#define CHA_ONACTIONSTOPPED	33
 
 // management stuff
 void scripting_state_init();
@@ -83,7 +86,7 @@ public:
 	int condition_type;
 	union
 	{
-		char name[NAME_LENGTH];
+		char name[CONDITION_LENGTH];
 	} data;
 
 	script_condition()

commit 35749dccccac802d07ac1922184bc5ae18be9a1d

Fixed breakage from r10254: key tokens in messages weren't getting translated right.
git-svn-id: svn://svn.icculus.org/fs2open/trunk/fs2_open@10293 387891d4-d844-0410-90c0-e4c51a9137d3

code/controlconfig/controlsconfigcommon.cpp
@@ -470,7 +470,7 @@ char *translate_key(char *key)
 
 	static char text[40] = {"None"};
 
-	index = translate_key_to_index(key, true);
+	index = translate_key_to_index(key, false);
 	if (index < 0) {
 		return NULL;
 	}